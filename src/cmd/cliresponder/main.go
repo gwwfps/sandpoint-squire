585041448b4c47a0f6d6fbef3bf48391976255a1
David Zhang <gwwfps@gmail.com>
2015-06-26 01:49:25 -0400

    fix input scanning

--- src/cmd/cliresponder/main.go
+++ src/cmd/cliresponder/main.go
@@ -1,18 +1,21 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"sqs/handlers"
 )
 
 func main() {
-	for {
-		fmt.Print("> ")
-		var input string
-		fmt.Scanln(&input)
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Print("> ")
+	for scanner.Scan() {
+		input := scanner.Text()
 		if input != "" {
 			response := handlers.HandleMessage(input)
 			fmt.Println(response)
 		}
+		fmt.Print("> ")
 	}
 }
